e75fca52a7301128e59f2ba1aa35b9050ba3ed6b
Supan Roy <supanroy2021@gmail.com>
2026-05-12 13:33:07 +0600

    add: field validations of product

--- fastapi-ecommerce/app/schema/product.py
+++ fastapi-ecommerce/app/schema/product.py
@@ -1,8 +1,15 @@
 from datetime import datetime
 from typing import Annotated, List, Optional
 from uuid import UUID
-
-from pydantic import BaseModel, Field, AnyUrl
+from pydantic import (
+    BaseModel,
+    Field,
+    AnyUrl,
+    field_validator,
+    model_validator,
+    computed_field,
+    EmailStr,
+)
 
 
 class Dimensions(BaseModel):
@@ -10,13 +17,30 @@ class Dimensions(BaseModel):
     width: float = Field(..., ge=0)
     height: float = Field(..., ge=0)
 
-
 class Seller(BaseModel):
-    seller_id: UUID
-    name: str
-    email: Optional[str] = None
-    website: Optional[str] = None
+    id: UUID
+    name: Annotated[
+        str,
+        Field(
+            min_length=2,
+            max_length=50,
+            title="Seller Name",
+            description="Name of the Seller (2-50 chars).",
+            examples=["Supan Roy", "Jonathan Byers", "Jupiter Sonic Labs Pvt Ltd."],
+        ),
+    ]
+    email: EmailStr
+    website: AnyUrl
 
+    @field_validator("email", mode="after")
+    @classmethod
+    def validate_seller_email_domain(cls, value: EmailStr):
+        allowed_domains = ["supanroy.com", "mistore.com", "apple.com", "amazon.com", "darazmall.com"]
+        domain = str(value).split("@"[-1]).lower()
+        if domain not in allowed_domains:
+            raise ValueError("Seller email domain not allowed: {domain}")
+
+        return value
 
 class Product(BaseModel):
     id: UUID
@@ -30,7 +54,16 @@ class Product(BaseModel):
             examples=["734-gxc-676-4h", "rfy6-6700-p342"],
         ),
     ]
-    name: str
+    name: Annotated[
+        str,
+        Field(
+            min_length=3,
+            max_length=80,
+            title="Product Name",
+            description="Readable product name (3-80 chars).",
+            examples=["Xiaomi Model Pro", "Apple Model X"],
+        ),
+    ]
     description: Optional[str] = None
     category: Optional[str] = None
     brand: Optional[str] = None
@@ -43,5 +76,39 @@ class Product(BaseModel):
     tags: Optional[List[str]] = None
     image_urls: Optional[List[AnyUrl]] = None
     dimensions_cm: Optional[Dimensions] = None
-    seller: Optional[Seller] = None
-    created_at: Optional[datetime] = None
+    seller: Seller
+    created_at: datetime
+
+    @field_validator("sku", mode="after")
+    @classmethod
+    def validate_sku_format(cls, value: str):
+        if "-" not in value:
+            raise ValueError("SKU must have '-'")
+
+        last = value.split("-"[-1])
+        if not (len(last) == 3 and last.isdigit()):
+            raise ValueError("SKU must end with a 3-digit sequence like -245")
+
+        return value
+    
+    @model_validator(mode="after")
+    @classmethod
+    def validate_business_rules(cls, model: "Product"):
+        if model.stock == 0 and model.is_active is True:
+            raise ValueError("If stock is 0, is_active must be false")
+        
+        if model.dicount_percent > 0 and model.rating == 0:
+            raise ValueError("Discounted product must have a rating more than zero")
+        
+        return model
+
+    @computed_field
+    @property
+    def final_price(self) -> float:
+        return round(self.price * (1-self.discount_percent/100), 2)
+    
+    @computed_field
+    @property
+    def volume_cm3(self) -> float:
+        d = self.dimensions_cm
+        return round(d.length * d.width * d.height, 2)
